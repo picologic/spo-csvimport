45828fe14b1394ebd889f3a913806c4e4904bb33
Glen Cooper <glenc@pico-logic.com>
2016-10-27 22:38:36 -0700

    Initial code

---
+++ SPOCsvImport/Options.cs
@@ -0,0 +1,47 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPOCsvImport
+{
+    [Verb("import", HelpText = "Import records from a csv file into a SharePoint list")]
+    public class ImportOptions
+    {
+        [Option('f', "file", Required = true, HelpText = "Data file containing data to import.")]
+        public string FilePath { get; set; }
+
+        [Option('r', "url", Required = true, HelpText = "Url to the site or tenant")]
+        public string Url { get; set; }
+
+        [Option('u', "username", Required = true, HelpText = "Username to authenticate")]
+        public string Username { get; set; }
+
+        [Option('p', "password", Required = true, HelpText = "Password to authenticate")]
+        public string Password { get; set; }
+
+        [Option('l', "list", Required = true, HelpText = "List to import to.")]
+        public string List { get; set; }
+
+        [Option('m', "map", Required = false, HelpText = "File containing CSV to List column mapping. If not provided assume 1:1 match")]
+        public string MappingFilePath { get; set; }
+    }
+
+    [Verb("clear", HelpText = "Remove all items from a SharePoint list.")]
+    public class ClearOptions
+    {
+        [Option('r', "url", Required = true, HelpText = "Url to the site or tenant")]
+        public string Url { get; set; }
+
+        [Option('u', "username", Required = true, HelpText = "Username to authenticate")]
+        public string Username { get; set; }
+
+        [Option('p', "password", Required = true, HelpText = "Password to authenticate")]
+        public string Password { get; set; }
+
+        [Option('l', "list", Required = false, HelpText = "List to remove items from")]
+        public string List { get; set; }
+    }
+}
---
+++ SPOCsvImport/Program.cs
@@ -0,0 +1,73 @@
+﻿using CommandLine;
+using DataAccess;
+using Microsoft.SharePoint.Client;
+using SPOCsvImport.Extensions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+
+namespace SPOCsvImport
+{
+    class Program
+    {
+        static int Main(string[] args) {
+            return CommandLine.Parser.Default.ParseArguments<ImportOptions, ClearOptions>(args)
+                .MapResult(
+                    (ImportOptions opts) => importData(opts),
+                    (ClearOptions opts) => clearItems(opts),
+                    errs => 1);
+        }
+
+        static int importData(ImportOptions opts) {
+            try {
+                // parse csv
+                var data = DataTable.New.ReadCsv(opts.FilePath);
+
+                // load mapping
+                IDictionary<string, string> mapping = null;
+                if (string.IsNullOrWhiteSpace(opts.MappingFilePath) == false){
+                    mapping = new Dictionary<string, string>();
+                    // load it
+                }
+
+                // initialize context
+                using (var ctx = new ClientContext(opts.Url)) {
+                    ctx.AuthenticationMode = ClientAuthenticationMode.Default;
+                    ctx.Credentials = getCredentials(opts.Username, opts.Password);
+
+                    var mgr = new ListManager(ctx, opts.List);
+                    mgr.ImportItems(data, mapping);
+                }
+            } catch (Exception ex) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+            return 0;
+        }
+
+        static int clearItems(ClearOptions opts) {
+            try {
+                // initialize context
+                using (var ctx = new ClientContext(opts.Url)) {
+                    ctx.AuthenticationMode = ClientAuthenticationMode.Default;
+                    ctx.Credentials = getCredentials(opts.Username, opts.Password);
+
+                    var mgr = new ListManager(ctx, opts.List);
+                    mgr.DeleteAllItems();
+                }
+            } catch (Exception ex) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+
+            return 0;
+        }
+
+        static ICredentials getCredentials(string username, string password) {
+            return new SharePointOnlineCredentials(username, password.ToSecureString());
+        }
+    }
+}
